0d7b598a9ccb62b9269fe660d36983d255d12070
gurungbsal18 <gurungbsal18@gmail.com>
2025-07-21 00:03:42 +0545

    mobile navbar implemented

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,7 +1,15 @@
+"use client";
 import Image from "next/image";
-import React from "react";
+import React, { useState } from "react";
+import { MdOutlineMenu, MdClose } from "react-icons/md";
 
 export default function Navbar() {
+  const [isOpen, setIsOpen] = useState(true);
+
+  const handleMobileMenuBtn = () => {
+    setIsOpen(!isOpen);
+  };
+
   return (
     <div>
       <div className="container flex items-center justify-between navbar py-2">
@@ -18,6 +26,29 @@ export default function Navbar() {
           <li>About us</li>
           <li>Contact us</li>
         </ul>
+
+        <div className="block md:hidden">
+          <MdOutlineMenu size={32} onClick={handleMobileMenuBtn} />
+          {!isOpen && (
+            <div className="bg-primary mobile-menu">
+              <div>
+                <ul>
+                  <li>Home</li>
+                  <li>Our Services</li>
+                  <li>About us</li>
+                  <li>Contact us</li>
+                </ul>
+              </div>
+
+              <div
+                className="bg-red-200 p-2 flex items-center justify-center text-red-700 rounded-full"
+                onClick={handleMobileMenuBtn}
+              >
+                <MdClose size={32} />
+              </div>
+            </div>
+          )}
+        </div>
       </div>
     </div>
   );
