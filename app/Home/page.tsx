695a09a3272828fa6ab0be5f531549e520c05372
gurungbsal18 <gurungbsal18@gmail.com>
2025-07-20 23:06:16 +0545

    hero section udpated

--- app/Home/page.tsx
+++ app/Home/page.tsx
@@ -1,10 +1,28 @@
+import { Button } from "@heroui/button";
 import React from "react";
+import { LuSearch, LuMail } from "react-icons/lu";
 
 export default function HomePage() {
+  const title = "Integrated Facilites Management For The Future";
   return (
-    <div>
+    <div className="hero">
       <div className="container mx-auto">
-        <h1>Welcome To AGGSFM</h1>
+        <div className="hero-content">
+          <h1 className="title text-white">{title}</h1>
+          <div className="flex items-start justify-start gap-4 w-full">
+            <Button className="" radius="sm" variant="shadow" color="primary">
+              <LuSearch /> Explore Services
+            </Button>
+            <Button
+              className="bg-white"
+              radius="sm"
+              variant="ghost"
+              color="primary"
+            >
+              <LuMail /> Contact Us
+            </Button>
+          </div>
+        </div>
       </div>
     </div>
   );
