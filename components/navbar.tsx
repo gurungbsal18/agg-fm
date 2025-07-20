017969b0833ef6d4ab1b30759d678942beeef301
gurungbsal18 <gurungbsal18@gmail.com>
2025-07-20 22:27:10 +0545

    topbar updated and scss added

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,9 +1,24 @@
+import Image from "next/image";
 import React from "react";
 
 export default function Navbar() {
   return (
     <div>
-      <h1>This is navbar</h1>
+      <div className="container flex items-center justify-between navbar py-2">
+        <Image
+          src="/aggfm-logo.svg"
+          alt="AGGFM"
+          width={1920}
+          height={1080}
+          className="logo-img"
+        />
+        <ul className="nav-item">
+          <li>Home</li>
+          <li>Our Services</li>
+          <li>About us</li>
+          <li>Contact us</li>
+        </ul>
+      </div>
     </div>
   );
 }
--- components/topbar.tsx
+++ components/topbar.tsx
@@ -1,10 +1,29 @@
 import React from "react";
 
 export default function TopBar() {
+  const data = [
+    {
+      id: 1,
+      content: "+60 12‑632 3277",
+      icon: "",
+    },
+    {
+      id: 2,
+      content: "contact@aggsfm.com",
+    },
+  ];
   return (
     <div className="bg-primary py-2 text-sm">
-      <div className="container">
-        <p>This is topbar</p>
+      <div className="container text-white">
+        <div className="flex gap-4">
+          {data.map((list) => {
+            return (
+              <div key={list.id}>
+                <p>{list.content}</p>
+              </div>
+            );
+          })}
+        </div>
       </div>
     </div>
   );
