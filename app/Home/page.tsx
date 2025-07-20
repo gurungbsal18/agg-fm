6bd08c187033e890cfb8237a801a1c66da43e7a6
gurungbsal18 <gurungbsal18@gmail.com>
2025-07-20 19:17:08 +0545

    initial push

---
+++ app/Home/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function HomePage() {
+  return (
+    <div>
+      <div className="container mx-auto">
+        <h1>Welcome To AGGSFM</h1>
+      </div>
+    </div>
+  );
+}
---
+++ components/topbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function TopBar() {
+  return (
+    <div className="bg-primary py-2 text-sm">
+      <div className="container">
+        <p>This is topbar</p>
+      </div>
+    </div>
+  );
+}
