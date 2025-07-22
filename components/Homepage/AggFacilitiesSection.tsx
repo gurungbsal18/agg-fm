0d75a662e2190c715b01c32804582225759295a4
gurungbsal18 <gurungbsal18@gmail.com>
2025-07-22 19:45:42 +0545

    link updated on homepage service section

--- components/Homepage/AggFacilitiesSection.tsx
+++ components/Homepage/AggFacilitiesSection.tsx
@@ -12,8 +12,16 @@ import { FaUserShield } from "react-icons/fa6";
 import { GoChecklist } from "react-icons/go";
 import { FaUsers } from "react-icons/fa";
 import { FaCar } from "react-icons/fa";
+import Link from "next/link";
 
-const serviceData = [
+interface serviceDataProps {
+  id: number;
+  title: string;
+  icon: React.ElementType;
+  link: string;
+}
+
+const serviceData: serviceDataProps[] = [
   {
     id: 1,
     title: "Building Maintenance",
@@ -30,46 +38,55 @@ const serviceData = [
     id: 3,
     title: "Cleaning Professionals",
     icon: MdCleaningServices,
+    link: "/",
   },
   {
     id: 4,
     title: "Swimming Pool Maintenance Services",
     icon: LiaSwimmingPoolSolid,
+    link: "/",
   },
   {
     id: 5,
     title: "Landscape Maintenance Services",
     icon: GrUserWorker,
+    link: "/",
   },
   {
     id: 6,
     title: "Mechanical, Electrical and Plumbing (MEP)",
     icon: HiOutlineWrenchScrewdriver,
+    link: "/",
   },
   {
     id: 7,
     title: "Defaulters Management and Recovery Services",
     icon: TbDatabaseStar,
+    link: "/",
   },
   {
     id: 8,
     title: "Security Guard Services",
     icon: FaUserShield,
+    link: "/",
   },
   {
     id: 9,
     title: "Inventory Fixed Assest Tagging Service",
     icon: GoChecklist,
+    link: "/",
   },
   {
     id: 10,
     title: "AGM/EGM Services",
     icon: FaUsers,
+    link: "/",
   },
   {
     id: 11,
     title: "Valet Parking Services",
     icon: FaCar,
+    link: "/",
   },
 ];
 
@@ -86,12 +103,14 @@ export default function AggFacilitiesSection() {
       <div className="grid grid-cols-1 md:grid-cols-3 gap-8 pt-8">
         {serviceData.map((list) => {
           return (
-            <div
-              key={list.id}
-              className="col-span-1 flex gap-2 items-center hover:rounded-sm hover:text-white hover:bg-primary px-4 py-2"
-            >
-              <list.icon size={24} className="custom-icon" />
-              <p>{list.title}</p>
+            <div key={list.id}>
+              <Link
+                href={list.link}
+                className="col-span-1 flex gap-2 items-center hover:rounded-sm hover:text-white hover:bg-primary px-4 py-2"
+              >
+                <list.icon size={24} className="custom-icon" />
+                <p>{list.title}</p>
+              </Link>
             </div>
           );
         })}
