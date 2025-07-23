c4f68a941365546febddff592ec4b5c7ee3258f0
gurungbsal18 <gurungbsal18@gmail.com>
2025-07-24 01:02:10 +0545

    fixed contact form UI

--- app/contact-us/page.tsx
+++ app/contact-us/page.tsx
@@ -10,37 +10,39 @@ export default function ContactPage() {
   return (
     <>
       <h1 className="title text-center text-primary">Let's Get In Touch</h1>
-      <div className="container bg-light-primary items-center py-16 mt-8">
-        <div className="grid grid-cols-3 primary">
-          <div className="col-span-1 flex flex-col gap-2 justify-center items-center">
-            <div className="flex items-center gap-2">
-              <span className="bg-primary p-2 text-white rounded-full">
-                <FaPhone size={24} />
-              </span>
-              <span className="bg-primary p-2 text-white rounded-full">
-                <BsWhatsapp size={24} />
-              </span>
+      <div className="bg-light-primary items-center py-16 mt-8">
+        <div className="container">
+          <div className="grid grid-cols-3 primary">
+            <div className="col-span-1 flex flex-col gap-2 justify-center items-center">
+              <div className="flex items-center gap-2">
+                <span className="bg-primary p-2 text-white rounded-full">
+                  <FaPhone size={24} />
+                </span>
+                <span className="bg-primary p-2 text-white rounded-full">
+                  <BsWhatsapp size={24} />
+                </span>
+              </div>
+              <Link href="https://wa.me/+60128907763">+60128907763</Link>
             </div>
-            <Link href="https://wa.me/+60128907763">+60128907763</Link>
-          </div>
-          <div className="col-span-1 flex flex-col gap-2 justify-center items-center">
-            <div className="flex items-center gap-2">
-              <span className="bg-primary p-2 text-white rounded-full">
-                <LuMail size={24} />
-              </span>
+            <div className="col-span-1 flex flex-col gap-2 justify-center items-center">
+              <div className="flex items-center gap-2">
+                <span className="bg-primary p-2 text-white rounded-full">
+                  <LuMail size={24} />
+                </span>
+              </div>
+              <Link href="mailto:contact@aggsfm.com">contact@aggsfm.com</Link>
             </div>
-            <Link href="mailto:contact@aggsfm.com">contact@aggsfm.com</Link>
-          </div>
-          <div className="col-span-1 flex flex-col gap-2 justify-center items-center">
-            <div className="flex items-center gap-2">
-              <span className="bg-primary p-2 text-white rounded-full">
-                <LuMail size={24} />
-              </span>
+            <div className="col-span-1 flex flex-col gap-2 justify-center items-center">
+              <div className="flex items-center gap-2">
+                <span className="bg-primary p-2 text-white rounded-full">
+                  <LuMail size={24} />
+                </span>
+              </div>
+              <p className="text-center">
+                15th Floor, Megan Legasi, Millerz Square, 357, Old Klang Rd,
+                58200 Kuala Lumpur, Malaysia
+              </p>
             </div>
-            <p className="text-center">
-              15th Floor, Megan Legasi, Millerz Square, 357, Old Klang Rd, 58200
-              Kuala Lumpur, Malaysia
-            </p>
           </div>
         </div>
       </div>
