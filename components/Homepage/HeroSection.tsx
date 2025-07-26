2bf075b60adc4a1bea8b572fc2de837ab1487554
gurungbsal18 <gurungbsal18@gmail.com>
2025-07-26 13:17:48 +0545

    animation added on hero title

--- components/Homepage/HeroSection.tsx
+++ components/Homepage/HeroSection.tsx
@@ -1,18 +1,50 @@
-import React from "react";
+"use client";
+
+import React, { useEffect, useRef } from "react";
 import { Button } from "@heroui/button";
 import { LuSearch, LuMail } from "react-icons/lu";
 
 export default function HeroSection() {
-  const title =
-    "AGG Facilities Management PLT As An Integrated Facilities Management For The Future";
+  const titleRef = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          entry.target.classList.add("animate");
+          observer.disconnect(); // Remove observer after animation runs once
+        }
+      },
+      {
+        threshold: 0.5, // Trigger when 50% is visible
+      }
+    );
+
+    if (titleRef.current) {
+      observer.observe(titleRef.current);
+    }
+
+    return () => {
+      if (titleRef.current) {
+        observer.unobserve(titleRef.current);
+      }
+    };
+  }, []);
 
   return (
     <div className="hero">
       <div className="container">
         <div className="hero-content">
-          <h1 className="title text-white text-center mb-4">{title}</h1>
+          <h1
+            ref={titleRef}
+            className="title text-white text-center mb-4 hero-title-animation"
+          >
+            AGG Facilities Management PLT <br />
+            As An Integrated Facilities Mangement <br />
+            For The Future
+          </h1>
           <div className="flex items-start justify-center gap-4 w-full">
-            <Button className="" radius="sm" variant="shadow" color="primary">
+            <Button radius="sm" variant="shadow" color="primary">
               <LuSearch /> Explore Services
             </Button>
             <Button radius="sm">
