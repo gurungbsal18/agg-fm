75ee0b94c34848cfa62eb32ee3ad6d7a3c330278
gurungbsal18 <gurungbsal18@gmail.com>
2025-07-26 10:55:37 +0545

    hero title changed as per requirement

--- components/Homepage/HeroSection.tsx
+++ components/Homepage/HeroSection.tsx
@@ -3,7 +3,7 @@ import { Button } from "@heroui/button";
 import { LuSearch, LuMail } from "react-icons/lu";
 
 export default function HeroSection() {
-  const title = "AGG Facilities Management PLT AS AN";
+  const title = "AGG FACILITIES MANAGEMENT PLT AS AN AGG";
 
   return (
     <div className="hero">
