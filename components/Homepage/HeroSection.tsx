fa379e266f1a94fc8f90db0c420c4b7d7b25329f
gurungbsal18 <gurungbsal18@gmail.com>
2025-07-26 12:43:22 +0545

    fixed hero div

--- components/Homepage/HeroSection.tsx
+++ components/Homepage/HeroSection.tsx
@@ -7,7 +7,7 @@ export default function HeroSection() {
 
   return (
     <div className="hero">
-      <div className="container mx-auto">
+      <div className="container">
         <div className="hero-content">
           <h1 className="title text-white text-center mb-4">{title}</h1>
           <div className="flex items-start justify-center gap-4 w-full">
