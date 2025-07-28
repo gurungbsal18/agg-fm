ed848b2c2ba583b23d092a3b4b4b22ad9119591a
gurungbsal18 <gurungbsal18@gmail.com>
2025-07-29 01:21:04 +0545

    extra content added

--- app/agm-egm-services/page.tsx
+++ app/agm-egm-services/page.tsx
@@ -1,4 +1,5 @@
 import PageHeader from "@/components/PageHeader";
+import Image from "next/image";
 import React from "react";
 
 export default function AgmEgmServicesPage() {
@@ -17,9 +18,7 @@ export default function AgmEgmServicesPage() {
           </p>
         </div>
 
-        <h4 className="text-primary font-bold">
-          Preparation and organization:
-        </h4>
+        <h4 className="font-bold">Preparation and organization:</h4>
         <div className="content">
           <p>
             This includes drafting notices, agendas, proxy forms, and other
@@ -27,39 +26,35 @@ export default function AgmEgmServicesPage() {
             printing/photocopying, enveloping and AR postages.
           </p>
         </div>
-        <h4 className="text-primary font-bold">
-          Logistics and administration:
-        </h4>
+        <h4 className="font-bold">Logistics and administration:</h4>
         <div className="content">
           <p>
             Managing venue booking, registration of attendees, and other
             logistical aspects of the meeting.
           </p>
         </div>
-        <h4 className="text-primary font-bold">Secretarial support:</h4>
+        <h4 className="font-bold">Secretarial support:</h4>
         <div className="content">
           <p>
             Providing assistance with the meeting proceedings, such as recording
             minutes, managing voting procedures, and handling Q&A sessions.
           </p>
         </div>
-        <h4 className="text-primary font-bold">
-          Compliance and regulatory requirements:
-        </h4>
+        <h4 className="font-bold">Compliance and regulatory requirements:</h4>
         <div className="content">
           <p>
             Ensuring that the meetings are conducted in accordance with company
             law, listing rules, and other relevant regulations.
           </p>
         </div>
-        <h4 className="text-primary font-bold">Virtual meeting solutions:</h4>
+        <h4 className="font-bold">Virtual meeting solutions:</h4>
         <div className="content">
           <p>
             Providing platforms and support for virtual or hybrid AGM/EGM
             setups, including registration, voting, and Q&A functionalities.
           </p>
         </div>
-        <h4 className="text-primary font-bold">Post-meeting support:</h4>
+        <h4 className="font-bold">Post-meeting support:</h4>
         <div className="content">
           <p>
             Assisting with the preparation and distribution of meeting minutes
@@ -69,7 +64,7 @@ export default function AgmEgmServicesPage() {
 
         <p className="font-bold">Example of service providers:</p>
 
-        <h4 className="text-primary font-bold">
+        <h4 className="font-bold">
           Secretarial services for JMB/MC monthly meetings:
         </h4>
         <div className="content">
@@ -78,14 +73,57 @@ export default function AgmEgmServicesPage() {
             our broader JMB/MC secretarial service offerings.
           </p>
         </div>
-        <h4 className="text-primary font-bold">
-          Specialized service providers:
-        </h4>
+        <h4 className="font-bold">Specialized service providers:</h4>
         <div className="content">
           <p>
             Our Company focus specifically on providing AGM/EGM support,
             including virtual meeting solutions and specialized software.
           </p>
+        </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-12 items-center gap-8 my-8">
+          <div className="col-span-12 md:col-span-3">
+            <Image
+              src="/images/agm-egm-service.webp"
+              alt="valet service"
+              width={1920}
+              height={1080}
+              className="shadow-2xl rounded-2xl"
+            />
+          </div>
+          <div className="col-span-12 md:col-span-9">
+            <h4 className="content-title">PRE AGM/EGM SERVICES FOR JMB/MC</h4>
+            <div className="content">
+              <h4 className="font-bold">Our Core Services</h4>
+              <ul className="list-disc ps-4">
+                <li>
+                  Print & Photocopy AGM/EGM notices as forwarded by MO via
+                  email.
+                </li>
+                <li>Place documents in a sealed envelope.</li>
+                <li>
+                  Print name and address on stickers and paste addressed
+                  stickers to envelope.
+                </li>
+                <li>
+                  Fill in AR (Acknowledgment of Receipt) card and post AR number
+                  on envelope and postage list.
+                </li>
+                <li>Submit at Post Laju service centre.</li>
+              </ul>
+
+              <h4 className="font-bold">Charges</h4>
+              <ul className="list-disc ps-4 mb-2">
+                <li>Standard printing and photocopying charges.</li>
+                <li>Standard AR postal charges as per weight and distance.</li>
+                <li>Enveloping and labour charges : RM 2.50 per address.</li>
+              </ul>
+              <p>
+                Note : We also take minutes of AGM/EGM at RM 800.00 unlimited
+                hours for JMB/MC
+              </p>
+            </div>
+          </div>
         </div>
       </div>
     </div>
--- app/valet-parking/page.tsx
+++ app/valet-parking/page.tsx
@@ -10,18 +10,72 @@ export default function ValetParkingPage() {
         image="/images/valet-parking.webp"
       />
       <div className="container animation-block">
-        <h4 className="content-title">Valet Parking</h4>
+        <div className="grid grid-cols-1 md:grid-cols-12 items-center gap-8 mb-8">
+          <div className="order-2 md:order-1 col-span-12 md:col-span-3">
+            <Image
+              src="/images/valet-service.webp"
+              alt="valet service"
+              width={1920}
+              height={1080}
+              className="shadow-2xl rounded-2xl"
+            />
+          </div>
+          <div className="order-1 md:order-2 col-span-12 md:col-span-9">
+            <h4 className="content-title">Valet Parking</h4>
+            <div className="content">
+              <p>
+                Get a fully managed valet parking service at your location. We
+                can customize our valet service to fit any parking locations or
+                events. We hire highly trained jockeys, insurance coverage and
+                charge very flexible pricing. Just drop your keys with the
+                jockey and our billing solutions will offer customers on per
+                hour or per entry billing after a specific time or day. This can
+                be implemented for low traffic locations and on weekends. We can
+                also customize monthly access for your highly valued customers.
+                This is normally implemented at office building areas.
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <h4 className="content-title">Seamless Service Operation</h4>
+
+        <h4 className="font-bold">Guests arrives</h4>
         <div className="content">
           <p>
-            Get a fully managed valet parking service at your location. We can
-            customize our valet service to fit any parking locations or events.
-            We hire highly trained jockeys, insurance coverage and charge very
-            flexible pricing. Just drop your keys with the jockey and our
-            billing solutions will offer customers on per hour or per entry
-            billing after a specific time or day. This can be implemented for
-            low traffic locations and on weekends. We can also customize monthly
-            access for your highly valued customers. This is normally
-            implemented at office building areas.
+            Valet offers a concierge service, welcoming all customers, saving
+            time and frustration of parking.
+          </p>
+        </div>
+
+        <h4 className="font-bold">Car checked-in seamlessly by valet</h4>
+        <div className="content">
+          <p>
+            Valet customers provide their phone number which allows the valet to
+            generate an SMS ticket sent directly to their phone immediately. Car
+            condition report captured and saved to cloud.
+          </p>
+          <p>
+            Check-in takes one to two minutes. Customers can enter the showroom
+            or service centre and relax in comfort.
+          </p>
+        </div>
+
+        <h4 className="font-bold">
+          Guest clicks on SMS link to retrive their car
+        </h4>
+        <div className="content">
+          <p>
+            Alert valet before leaving, avoiding waiting time in the car park.
+          </p>
+        </div>
+
+        <h4 className="font-bold">Car is ready and waiting</h4>
+        <div className="content">
+          <p>
+            Customer arrives at valet stand and their car is ready and waiting
+            for them, having been checked for any damage. Ticket number is
+            verified on their phone. Customer departs venue.
           </p>
         </div>
       </div>
