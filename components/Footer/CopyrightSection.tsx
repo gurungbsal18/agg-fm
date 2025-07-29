b7df094a0530a719527b1f749694fe7785cda0a5
gurungbsal18 <gurungbsal18@gmail.com>
2025-07-29 18:10:19 +0545

    footer year and link updated

--- components/Footer/CopyrightSection.tsx
+++ components/Footer/CopyrightSection.tsx
@@ -1,11 +1,21 @@
+import Link from "next/link";
 import React from "react";
 
 export default function CopyrightSection() {
+  const currentYear = new Date().getFullYear();
   return (
     <div className="container py-2 text-gray-500">
       <div className="flex flex-col md:flex-row justify-center items-center md:justify-between gap-1">
-        <p className="text-xs">2025 AGGSFM. All rights reserved</p>
-        <p className="text-xs">Developed by: IT Sansaar</p>
+        <p className="text-xs">
+          &copy; {currentYear} AGGSFM. All rights reserved
+        </p>
+        <Link
+          href="https://www.itsansaar.com.np/"
+          target="_blank"
+          className="hover:text-primary hover:underline"
+        >
+          <p className="text-xs">Developed by: IT Sansaar</p>
+        </Link>
       </div>
     </div>
   );
