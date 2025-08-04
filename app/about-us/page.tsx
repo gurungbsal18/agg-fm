8c513fa8aef7cfb9b6fe421767df896ba90c297f
gurungbsal18 <gurungbsal18@gmail.com>
2025-08-04 21:24:32 +0545

    our team page created

--- app/about-us/page.tsx
+++ app/about-us/page.tsx
@@ -54,7 +54,6 @@ export default function AboutUsPage() {
           </p>
         </div>
 
-        <OurTeam />
       </div>
     </>
   );
---
+++ app/our-team/page.tsx
@@ -0,0 +1,13 @@
+import OurTeam from '@/components/ourTeam/OurTeam'
+import PageHeader from '@/components/PageHeader'
+import React from 'react'
+
+export default function OurTeamPage() {
+  return (
+    <div>
+        <PageHeader title='Our Team' />
+                <OurTeam />
+
+    </div>
+  )
+}
